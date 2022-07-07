27307d95cc91db32f019eacaed09b664681f765d
Tran Minh Tan <92872821+MinhTan77@users.noreply.github.com>
2022-07-07 14:42:20 +0700

    add DataStorage

---
+++ ChatApplication/Data/DataStorage.cs
@@ -0,0 +1,21 @@
+﻿namespace ChatApplication.Data
+{
+    internal class DataStorage
+    {
+        private DataStorage storage { get; set; }
+        private DataStorage()
+        {
+            
+        }
+        public DataStorage GetDataStorage()
+        {
+            if (storage == null)
+            {
+                storage = new DataStorage();
+
+            }
+
+            return storage;
+        }
+    }
+}
--- ChatApplication/Models/PrivateGroup.cs
+++ ChatApplication/Models/PrivateGroup.cs
@@ -3,6 +3,7 @@
     public class PrivateGroup : Group
     {
         public int Id { get; set; }
+        public User Admin { get; set; }
         public List<User> UsersList { get; set; }
        
     }
