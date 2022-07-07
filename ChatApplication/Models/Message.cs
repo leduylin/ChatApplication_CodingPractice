54ca285b7d3f04024e6af22aa48d04bcaa939a93
leduylin <lin.le.set18@eiu.edu.vn>
2022-07-07 14:11:33 +0700

    create message class

---
+++ ChatApplication/Models/Message.cs
@@ -0,0 +1,10 @@
+﻿namespace ChatApplication.Models
+{
+    public class Message
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public IFormFile FormFile { get; set; }
+
+    }
+}
